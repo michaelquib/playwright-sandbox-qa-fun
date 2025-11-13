068cc368ff71164a289740d0cf8b0c35162495aa
michaelquib <michaelquiban@proton.me>
2025-11-12 20:11:46 -0800

    Navigate to site, click docs, expect a url, then check it lands correctly on the installation page

--- tests/inner-Navigation-Bar.spec.ts
+++ tests/inner-Navigation-Bar.spec.ts
@@ -1,7 +1,14 @@
 import {test, expect} from "@playwright/test";
 
-//Select the Docs button from the navigation bar and verify it goes to the correct page
+/*
+The logic behind this test is we're doing a few things and making some assumptions.
+1. Navigate to the Playwright website.
+2. Click on the "Docs" button in the navigation bar.
+3. Verify that the URL is correct.
+4. Verify that we landed on the correct page by checking for specific headings.
 
+I'm sure we can expand this test further, but this is a good start.
+*/
 test("Select the docs button and verify it goes to the next page", async ({page}) => {
     //Navigate to the Playwright website
      await page.goto("https://playwright.dev/");
@@ -11,6 +18,13 @@ test("Select the docs button and verify it goes to the next page", async ({page}
 
     //Verify that the URL is correct
     await expect(page).toHaveURL("https://playwright.dev/docs/intro");
+
+    //Verify it landed on the correct page by checking for a two headings
+    const installationHeading = page.locator('h1', { hasText: 'Installation' });
+    await expect(installationHeading).toBeVisible();
+
+    const introductionHeading = page.locator('h2', { hasText: 'Introduction' });
+    await expect(introductionHeading).toBeVisible();
 });
 
 
